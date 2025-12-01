6e43c1f5f15a24509e343b8054c73b33fb55da58
ninalining <168672412+ninalining@users.noreply.github.com>
2025-12-01 22:54:42 +0100

    Fix rotation logic and solve Part 2 python

--- python/day01.py
+++ python/day01.py
@@ -14,11 +14,16 @@ counter = 0
 for line in lines:
     direction = line[0]
     amount = int(line[1:])
+    counter += amount // 100
+    remainder = amount % 100
+
     if direction == 'L':
-        position = (position - amount) % 100
+        if position != 0 and position - remainder <= 0:
+            counter += 1
+        position = (position - remainder) % 100
     elif direction == 'R':
-        position = (position + amount) % 100
-    if position == 0:
-        counter += 1
+        if position + remainder >= 100:
+            counter += 1
+        position = (position + remainder) % 100
 
 print(f"Password: {counter}")
